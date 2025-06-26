3a46384c4e4580203fc3c1eea292d47e7f2f376a
RWCannell <regancannell@gmail.com>
2025-06-26 15:55:59 +0200

    Added some bash script for convenience

---
+++ bash_scripts/create_fence_config.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+kubectl delete secret fence-config
+kubectl create secret generic fence-config --from-file=fence-config.yaml
+sleep 5
+kubectl rollout restart deployment fence-deployment
